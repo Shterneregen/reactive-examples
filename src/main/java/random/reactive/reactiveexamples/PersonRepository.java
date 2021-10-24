e6ee5967696a84356cacb6f592335015e78ba103
shterneregen <shterneregen@gmail.com>
2021-10-25 00:55:36 +0300

    init commit

---
+++ src/main/java/random/reactive/reactiveexamples/PersonRepository.java
@@ -0,0 +1,12 @@
+package random.reactive.reactiveexamples;
+
+import random.reactive.reactiveexamples.domain.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface PersonRepository {
+
+    Mono<Person> getById(Integer id);
+
+    Flux<Person> findAll();
+}
