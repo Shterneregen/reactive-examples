ac8822a6e77987d388d7130dee295a9d785ba88d
shterneregen <shterneregen@gmail.com>
2021-10-25 22:18:40 +0300

    init netflux app

---
+++ netflux/src/test/java/random/netflux/NetfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package random.netflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
