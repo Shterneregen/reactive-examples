167ef4f859a0cc18e5d7e7a33bc13c79382c2e1c
shterneregen <shterneregen@gmail.com>
2021-10-25 23:17:17 +0300

    adding stream events example

--- netflux/src/main/java/random/netflux/domain/MovieController.java
+++ netflux/src/main/java/random/netflux/domain/MovieController.java
@@ -1,6 +1,7 @@
 package random.netflux.domain;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +25,10 @@ public class MovieController {
     @GetMapping
     Flux<Movie> getAllMovies() {
         return movieService.getAllMovies();
+    }
+
+    @GetMapping(value = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<MovieEvent> streamMovieEvents(@PathVariable String id) {
+        return movieService.streamMovieEvents(id);
     }
 }
--- netflux/src/main/java/random/netflux/services/MovieServiceImpl.java
+++ netflux/src/main/java/random/netflux/services/MovieServiceImpl.java
@@ -3,9 +3,13 @@ package random.netflux.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import random.netflux.domain.Movie;
+import random.netflux.domain.MovieEvent;
 import random.netflux.repositories.MovieRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +25,16 @@ public class MovieServiceImpl implements MovieService {
     @Override
     public Flux<Movie> getAllMovies() {
         return movieRepository.findAll();
+    }
+
+    /**
+     * This is going to emit a new movie event every second.
+     * We are using the SynchronousSink to create a continuous stream of movie events.
+     */
+    @Override
+    public Flux<MovieEvent> streamMovieEvents(String id) {
+        return Flux.<MovieEvent>generate(movieEventSynchronousSink -> {
+            movieEventSynchronousSink.next(new MovieEvent(id, new Date()));
+        }).delayElements(Duration.ofSeconds(1));
     }
 }
