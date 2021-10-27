98fe0fbd7a58aaab96ec15c1f5198e4e68c38cd3
shterneregen <shterneregen@gmail.com>
2021-10-27 23:27:08 +0300

    init webflux-client

---
+++ webflux-client/src/main/java/random/webfluxclient/WebfluxClientApplication.java
@@ -0,0 +1,11 @@
+package random.webfluxclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxClientApplication.class, args);
+    }
+}
