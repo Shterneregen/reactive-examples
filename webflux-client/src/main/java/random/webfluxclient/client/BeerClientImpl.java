2811f14a425cf5dfe69aebab3e7a2a95850260f6
shterneregen <shterneregen@gmail.com>
2021-10-31 23:31:03 +0300

    adding webflux-client mock impl

---
+++ webflux-client/src/main/java/random/webfluxclient/client/BeerClientImpl.java
@@ -0,0 +1,42 @@
+package random.webfluxclient.client;
+
+import org.springframework.http.ResponseEntity;
+import random.webfluxclient.model.BeerDto;
+import random.webfluxclient.model.BeerPagedList;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+public class BeerClientImpl implements BeerClient {
+
+    @Override
+    public Mono<BeerDto> getBeerById(UUID id, Boolean showInventoryOnHand) {
+        return null;
+    }
+
+    @Override
+    public Mono<BeerPagedList> listBeers(
+            Integer pageNumber, Integer pageSize, String beerName, String beerStyle, Boolean showInventoryOnhand) {
+        return null;
+    }
+
+    @Override
+    public Mono<ResponseEntity> createBeer(BeerDto beerDto) {
+        return null;
+    }
+
+    @Override
+    public Mono<ResponseEntity> updateBeer(BeerDto beerDto) {
+        return null;
+    }
+
+    @Override
+    public Mono<ResponseEntity> deleteBeerById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Mono<BeerDto> getBeerByUPC(String upc) {
+        return null;
+    }
+}
