826c9807e36bcd8f2e24be7a3b0fbb0807cb52a3
shterneregen <shterneregen@gmail.com>
2021-11-12 00:31:11 +0300

    adding quote service

---
+++ streaming-stock-quote-service/src/main/java/random/streamingstockquoteservice/service/QuoteGeneratorService.java
@@ -0,0 +1,11 @@
+package random.streamingstockquoteservice.service;
+
+import random.streamingstockquoteservice.model.Quote;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public interface QuoteGeneratorService {
+
+    Flux<Quote> fetchQuoteStream(Duration period);
+}
